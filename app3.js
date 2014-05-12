38eb30e0bbcfa426df9499402302783c0f48d97f
Summer <summermichiko@gmail.com>
2014-05-12 14:07:29 -0400

    initial commit

---
+++ app3.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	$("#li-one").mouseover(function() {
+		$(this).css("background-color", "#FFFFFF");
+	})
+	$("#li-two").mouseover(function() {
+		$(this).css("background-color", "#FFFFFF");
+	})
+	$("#li-four").mouseover(function() {
+		$(this).css("background-color", "#FFFFFF");
+	})
+	$("#li-five").mouseover(function() {
+		$(this).css("background-color", "#FFFFFF");
+	})
+	$("#li-six").mouseover(function() {
+		$(this).css("background-color", "#FFFFFF");
+	})
+	$("#li-one").mouseleave(function() {
+		$(this).css("background-color", "#F1F1F1");
+	})
+	$("#li-two").mouseleave(function() {
+		$(this).css("background-color", "#F1F1F1");
+	})
+	$("#li-four").mouseleave(function() {
+		$(this).css("background-color", "#F1F1F1");
+	})
+	$("#li-five").mouseleave(function() {
+		$(this).css("background-color", "#F1F1F1");
+	})
+	$("#li-six").mouseleave(function() {
+		$(this).css("background-color", "#F1F1F1");
+	})
+	$("#li-one").click(function() {
+		location.href = "li-one.html"
+	})
+	$("#li-two").click(function() {
+		location.href = "li-two.html"
+	})
+	$("#li-four").click(function() {
+		location.href = "li-four.html"
+	})
+	$("#li-five").click(function() {
+		location.href = "li-five.html"
+	})
+	$("#li-six").click(function() {
+		location.href = "li-six.html"
+	})
+});
